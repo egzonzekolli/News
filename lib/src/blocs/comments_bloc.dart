8adac66fecb3888c34bc97d4cce7f7c8eba0cdbc
egzonzekolli <egzonzekolli100@gmail.com>
2021-01-26 10:06:47 +0100

    new

---
+++ lib/src/blocs/comments_bloc.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:rxdart/futures.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/subjects.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class CommentsBloc {
+  final _repository = Repository();
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  //Strems
+  Observable<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  //Sink
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  CommentsBloc() {
+    _commentsFetcher.stream
+        .transform(_commentsTransforemer())
+        .pipe(_commentsOutput);
+  }
+
+  _commentsTransforemer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+      (cache, int id, index) {
+        cache[id] = _repository.fetchItem(id);
+        cache[id].then((ItemModel item) {
+          item.kids.forEach((kidId) => fetchItemWithComments(kidId));
+        });
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
